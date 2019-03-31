031fb902a4c51c62b03c0cf99911883df6de4f18
ilandroxxy <ilyand98@mail.ru>
2019-04-01 00:34:41 +0700

    fixed main.c from master

--- main.c
+++ main.c
@@ -18,16 +18,12 @@ int main()
 {
     int arr[] = {3, 1, 4, 1, 5, 9, 2};
 
-    const int min = min_element(arr, 7);
-
-    printf("Array: ");
-    int i;
-    for (i = 0; i < 7; ++i) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+    const int min = min_element(arr, N_ELEMENTS(arr));
 
     printf("Minimum element: %d\n", min);
+
+    print_array(arr,N_ELEMENTS(arr));
+    printf("Min element: %d\n", min);
 
     return 0;
 }
